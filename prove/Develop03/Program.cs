678378bfb2c0eff9b036b8e3af9fb92804b0b07b
StardustPaladin <Eheroneos55@gmail.com>
2025-10-01 16:09:52 -0600

    created the class word

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -34,4 +34,42 @@ class Reference
         return $"{_book} {_chapter}:{_startVerse}-{_endVerse}";
     }
 }
+class Word
+{
+    private string _text;
+    private bool _isHidden;
 
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public override string ToString()
+    {
+        if (!_isHidden) return _text;
+
+        // show underscores for letters, keep punctuation
+        char[] chars = new char[_text.Length];
+        for (int i = 0; i < _text.Length; i++)
+        {
+            chars[i] = char.IsLetter(_text[i]) ? '_' : _text[i];
+        }
+        return new string(chars);
+    }
+}
