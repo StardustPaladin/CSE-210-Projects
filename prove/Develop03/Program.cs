92cb0a5a7a4c66038bb262b07a359347de1a6a20
StardustPaladin <Eheroneos55@gmail.com>
2025-10-01 16:08:45 -0600

    Added the Reference class for the Scripture memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+// Very simple Scripture memorizer for beginners.
+// Classes: Reference, Word, Scripture, Program
+
+class Reference
 {
-    static void Main(string[] args)
+    private string _book;
+    private int _chapter;
+    private int _startVerse;
+    private int _endVerse;
+
+    public Reference(string book, int chapter, int verse)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        _book = book;
+        _chapter = chapter;
+        _startVerse = verse;
+        _endVerse = verse;
+    }
+
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = startVerse;
+        _endVerse = endVerse;
+    }
+
+    public override string ToString()
+    {
+        if (_startVerse == _endVerse)
+            return $"{_book} {_chapter}:{_startVerse}";
+        return $"{_book} {_chapter}:{_startVerse}-{_endVerse}";
     }
 }
+
