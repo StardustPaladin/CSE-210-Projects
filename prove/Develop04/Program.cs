5c44b63ac28415bb2d65940590643544fd453130
StardustPaladin <Eheroneos55@gmail.com>
2025-10-09 08:33:37 -0600

    Started Mindfullness program

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,67 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
-class Program
+abstract class Activity
 {
-    static void Main(string[] args)
+    private string _name;
+    private string _description;
+    private int _durationSeconds;
+
+    public Activity(string name, string description)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        _name = name;
+        _description = description;
+    }
+
+    public void Start()
+    {
+        Console.Clear();
+        Console.WriteLine($"Starting: {_name}");
+        Console.WriteLine(_description);
+        Console.Write("How many seconds would you like for this session? ");
+        if (int.TryParse(Console.ReadLine(), out int seconds))
+        {
+            _durationSeconds = seconds;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input, defaulting to 30 seconds.");
+            _durationSeconds = 30;
+        }
+
+        Console.WriteLine("Get ready...");
+        Spinner(3);
+        RunActivity(_durationSeconds);
+        Console.WriteLine();
+        Console.WriteLine("Well done! You have completed the activity.");
+        Console.WriteLine($"Activity: {_name} | Duration: {_durationSeconds} seconds");
+        Spinner(3);
+    }
+
+    protected abstract void RunActivity(int durationSeconds);
+
+    protected void Spinner(int seconds)
+    {
+        string[] seq = { "|", "/", "-", "\\" };
+        int total = seconds * 4;
+        for (int i = 0; i < total; i++)
+        {
+            Console.Write(seq[i % seq.Length]);
+            Thread.Sleep(250);
+            Console.Write('\r');
+        }
+    }
+
+    protected void Countdown(int seconds)
+    {
+        for (int i = seconds; i >= 1; i--)
+        {
+            Console.Write(i + " ");
+            Thread.Sleep(1000);
+            Console.Write('\r');
+        }
+        Console.Write(new string(' ', 10) + "\r");
     }
 }
+
