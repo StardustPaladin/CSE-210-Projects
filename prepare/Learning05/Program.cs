122d3193e3490fb672b958ab291008e42d29ed00
StardustPaladin <Eheroneos55@gmail.com>
2025-10-13 08:30:35 -0600

    wrote the program file

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        var shapes = new List<Shape>
+        {
+            new Square("red", 4),
+            new Rectangle("blue", 3, 5),
+            new Circle("green", 2.5)
+        };
+
+        foreach (var s in shapes)
+        {
+            Console.WriteLine($"Color: {s.GetColor()}, Area: {s.GetArea():F2}");
+        }
     }
 }
