a513c8b4c3ca99bcc7a68cd1b46b2c06f0ccb8bc
StardustPaladin <Eheroneos55@gmail.com>
2025-10-27 08:30:24 -0600

    added the rest of the program and added a few finishing touches

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Foundation4
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        static void Main(string[] args)
+        {
+            var activities = new List<Activity>();
+
+            // Create one of each activity (using miles)
+            activities.Add(new RunningActivity("03 Nov 2022", 30, 3.0)); // 3.0 miles in 30 min
+            activities.Add(new CyclingActivity("03 Nov 2022", 30, 12.0)); // 12 mph average for 30 min
+            activities.Add(new SwimmingActivity("03 Nov 2022", 30, 20)); // 20 laps in 30 min
+
+            // Display summaries
+            foreach (var act in activities)
+            {
+                Console.WriteLine(act.GetSummary());
+            }
+        }
     }
 }
