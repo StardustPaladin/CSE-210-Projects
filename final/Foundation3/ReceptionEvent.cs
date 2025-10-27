2209817e0a4e135caf3618fa5497477bebb5f546
StardustPaladin <Eheroneos55@gmail.com>
2025-10-27 08:23:10 -0600

    added the recptions event class

---
+++ final/Foundation3/ReceptionEvent.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Foundation3
+{
+    public class ReceptionEvent : Event
+    {
+        private string _rsvpEmail;
+
+        public ReceptionEvent(string title, string description, string date, string time, Address address, string rsvpEmail)
+            : base(title, description, date, time, address)
+        {
+            _rsvpEmail = rsvpEmail;
+        }
+
+        public override string GetFullDetails()
+        {
+            return $"{base.GetStandardDetails()}\nType: Reception\nRSVP Email: {_rsvpEmail}";
+        }
+    }
+}
